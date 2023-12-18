f08c4df998dbe49bf358f1552e57ac72169e6cb3
Scott Howson <scott_howson@corruptiongames.net>
2023-12-18 08:27:21 -0500

    Updated Sources of chroot files

--- fetch-instantnoodle-recovery-files.sh
+++ fetch-instantnoodle-recovery-files.sh
@@ -25,22 +25,23 @@ download_files() {
 }
 
 clone_repository() {
-    if [ ! -d "Instantnoodle-Extras/Halium-Boot" ]; then
-        mkdir -p Instantnoodle-Extras/Halium-Boot || { echo -e "\e[91mFailed to create directory\e[0m"; exit 1; }
-        git clone https://github.com/Halium/halium-boot.git Instantnoodle-Extras/Halium-Boot || { echo -e "\e[91mFailed to clone repository\e[0m"; exit 1; }
-        wget -O Instantnoodle-Extras/Halium-Boot/instantnoodle_user_defconfig https://raw.githubusercontent.com/IllSaft/halium_kernel_oneplus_sm8250/halium-11.0/arch/arm64/configs/vendor/instantnoodle_user_defconfig
-        wget -O Instantnoodle-Extras/Chroot/chroot.sh https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/chroot.sh
-        wget -O Instantnoodle-Extras/Chroot/README.md https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/README.md
-        wget -O Instantnoodle-Extras/Chroot/Instantnoodle-Info.log https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/Instantnoodle-Info
-        wget -O Instantnoodle-Extras/Recovery/Instantnoodle-Recovery.log https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/Instantnoodle-Recovery.log
-        rm Instantnoodle-Extras/Halium-Boot/Android.mk Instantnoodle-Extras/Halium-Boot/get-initrd.sh Instantnoodle-Extras/Halium-Boot/LICENSE Instantnoodle-Extras/Halium-Boot/README.md
+    if [ ! -d "instantnoodle-extras/halium-boot" ]; then
+        mkdir -p instantnoodle-extras/halium-boot || { echo -e "\e[91mFailed to create directory\e[0m"; exit 1; }
+        git clone https://github.com/Halium/halium-boot.git instantnoodle-extras/halium-boot || { echo -e "\e[91mFailed to clone repository\e[0m"; exit 1; }
+        wget -O instantnoodle-extras/halium-boot/instantnoodle_user_defconfig https://raw.githubusercontent.com/IllSaft/halium_kernel_oneplus_sm8250/halium-11.0/arch/arm64/configs/vendor/instantnoodle_user_defconfig
+        wget -O instantnoodle-extras/chroot/chroot-log-data.sh https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/chroot-log-data.sh
+        wget -O instantnoodle-extras/chroot/chroot-log-system.sh https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/chroot-log-system.sh
+        wget -O instantnoodle-extras/chroot/README.md https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/README.md
+        wget -O instantnoodle-extras/chroot/Instantnoodle-Info.log https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/Instantnoodle-Info
+        wget -O instantnoodle-extras/recovery/Instantnoodle-Recovery.log https://raw.githubusercontent.com/IllSaft/instantnoodle-chroot/main/Instantnoodle-Recovery.log
+        rm instantnoodle-extras/halium-boot/Android.mk instantnoodle-extras/halium-boot/get-initrd.sh instantnoodle-extras/halium-boot/LICENSE instantnoodle-extras/halium-boot/README.md
         echo -e "\e[92mRepository cloned successfully.\e[0m"
-        echo -e "\e[96mRepository cloned to: $(pwd)/Instantnoodle-Extras/Halium-Boot\e[0m"
+        echo -e "\e[96mRepository cloned to: $(pwd)/instantnoodle-extras/halium-boot\e[0m"
     else
         read -p "Repository already exists. Would you like to re-clone? (y/n): " choice
         case "$choice" in
             [Yy]*)
-                rm -rf Instantnoodle-Extras/Halium-Boot
+                rm -rf instantnoodle-extras/halium-boot
                 clone_repository
                 ;;
             [Nn]*)
@@ -54,8 +55,8 @@ clone_repository() {
 }
 
 main() {
-    mkdir -p Instantnoodle-Extras/Halium-Boot Instantnoodle-Extras/Chroot Instantnoodle-Extras/Recovery || { echo -e "\e[91mFailed to create directories\e[0m"; exit 1; }
-    cd Instantnoodle-Extras/Recovery || { echo -e "\e[91mFailed to change directory\e[0m"; exit 1; }
+    mkdir -p instantnoodle-extras/halium-boot instantnoodle-extras/chroot instantnoodle-extras/recovery || { echo -e "\e[91mFailed to create directories\e[0m"; exit 1; }
+    cd instantnoodle-extras/recovery || { echo -e "\e[91mFailed to change directory\e[0m"; exit 1; }
     download_files || { echo -e "\e[91mFailed to download files\e[0m"; exit 1; }
     cd "$OLDPWD" || { echo -e "\e[91mFailed to change back to the original directory\e[0m"; exit 1; }
     clone_repository || { echo -e "\e[91mFailed to clone repository\e[0m"; exit 1; }
